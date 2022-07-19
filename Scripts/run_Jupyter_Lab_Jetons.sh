69b71a4cc7222481da4689c1e5a83888c9e86925
James Radtke <cloudxabide@gmail.com>
2022-07-19 14:22:19 -0400

    Adding params

--- Scripts/run_Jupyter_Lab_Jetons.sh
+++ Scripts/run_Jupyter_Lab_Jetons.sh
@@ -7,9 +7,22 @@
 export CONTAINER_NAME="l4t-ml:r32.6.1-py3"
 
 # OPTIONS
-JUPYTER_TOKEN="InsertToken Here"
-JUPYTER_ENABLE_LAB=yes
-CHOWN_HOME=yes
+export JUPYTER_TOKEN="notAPassword"
+export JUPYTER_ENABLE_LAB=yes
+export CHOWN_HOME=yes
+export USER=root
+export NB_USER=nvidia
+export WORK_PATH=/home/nvidia/work/
+export NOTEBOOKS_PATH=/home/nvidia/Notebooks/
+
+run_me() {
+  sudo docker pull nvcr.io/nvidia/${CONTAINER_NAME}
+  sudo docker run -it --rm --runtime nvidia --network host -p 8888:8888 \
+	  --user $USER -e NB_USER=$NB_USER \
+	  -e CHOWN_HOME=$CHOWN_HOME -e JUPYTER_ENABLE_LAB=$JUPYTER_ENABLE_LAB -e JUPYTER_TOKEN=$JUPYTER_TOKEN \
+	  -v ${NOTEBOOKS_PATH}:/Notebooks -v ${WORK_PATH}:/work \
+	  nvcr.io/nvidia/${CONTAINER_NAME}
+}
 
 run_me() {
   sudo docker pull nvcr.io/nvidia/${CONTAINER_NAME}
